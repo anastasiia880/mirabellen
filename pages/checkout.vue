2e91b560c067107df69f47bdf3138748e8174076
Anastasiia Shevtsova <anastasiia08@ukr.net>
2025-06-13 11:16:16 +0200

    Update checkout page.

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -2,7 +2,7 @@
   <div
     class="flex h-screen items-center justify-center font-serif text-[36px] text-stone-500 md:text-[40px]"
   >
-    [ ...your products... ]
+    [ ...your products list... ]
   </div>
 </template>
 
